4cb130d747eee98391e662ed34f0ece0849e080c
Guest Arf <ohbabyatripleohyeaah@gmail.com>
2024-10-07 17:29:41 -0400

    there i finished

--- ggg_lol.py
+++ ggg_lol.py
@@ -1,6 +1,15 @@
+#please note that none of the insults in this project are real
 import time
 time.sleep(1)
 #haha
 #i am a person
 print("yes i exist hi")
 print("the second")
+#step 1: git clone (clones repo, duh)
+#step 2: open project in vs code
+#step 3: write some random code or make a file (doesn't matter)
+#step 4: save code
+#step 5: git status (checks your file changes in comparison to the github repo)
+#step 6: git add .
+#step 7: git commit -m "input your message here silly"
+#step 8: git push (change will appear on github account)
